14d9a5d104debd7f8f41fc3567d8000f23613bbd
John <khamidjonashuraliev033@gmail.com>
2024-02-24 02:21:29 +0900

    feat: Arraylar

--- js/script.js
+++ js/script.js
@@ -1,28 +1,53 @@
-// scope va context - object
+//Array Methods
+// const numbers = [5, 6, 11, 32, 2];
+//reverse
+// numbers.reverse();
+// console.log(numbers);
 
-const person = {
-    ism: "john",
-    yosh: 30,
-    millati: "uzbek",
+// sort
+// numbers.sort(function(a, b) {return a - b});
+// console.log(numbers);
 
-    salom_berin() {
-        console.log("assalomu alaykum");
-    },
-    hayrlashing() {
-        console.log("salomat bo'ling");
-    },
-    tonishtiring() {
+// numbers.sort(function(a, b) {return  b - a});
+// console.log(numbers);
 
-        const action = () =>  {
-            console.log(this);
-            const name = this.ism;
-            console.log(name);
+// ITERATION METHODS
+// forEach
+// const list = ["olma", "anor", "tarvuz"];
+// const numbers = [5, 6, 11, 32, 2];
+// let numberM = [];
+// numbers.forEach((ele) => {
+//     console.log("son:", ele);
+//     numberM.push(ele * 10);
+// });
+//  console.log("numberM:", numberM);
 
-        }
-        action();
-        console.log(
-            `mening ismim ${this.ism} va mening millatim ${this.millati}`
-        );
-    },
-};
-person.tonishtiring();
+
+//map
+// const numbers = [5, 6, 11, 32, 2];
+// let numberM = numbers.map((ele) => (ele*30));
+// console.log("numberM:", numberM);
+
+
+// filter 
+// const list = ["olma", "anor", "tarvuz"];
+// let new_list = list.filter((ele) => {
+//     return ele !== "anor";
+
+// });
+// console.log("new_list:", new_list);
+
+// const list = ["olma", "anor", "tarvuz"];
+// let new_list = list.filter((ele) => {
+//     return ele === "anor";
+
+// });
+// console.log("new_list:", new_list);
+
+// reduse
+// const numbers = [5, 6, 11, 32, 2];
+// let new_summ = numbers.reduce((total, ele) => {
+//     return total * ele;
+
+// });
+// console.log("new_summ:", new_summ);
